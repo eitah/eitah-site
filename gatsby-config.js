31310db2e8ccdf76d2a30dc968404e7f126130b8
Elijah Itah <elijahbit@gmail.com>
2018-09-02 22:38:33 -0500

    Updating template slightly for header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Eli Itah',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,7 @@ const Header = ({ siteTitle }) => (
     style={{
       background: 'rebeccapurple',
       marginBottom: '1.45rem',
+      'text-align': 'center',
     }}
   >
     <div
